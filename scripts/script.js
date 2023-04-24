c9661eeb9992cd6d0fd2415e1dc870945d60d492
Energizet <a2437@yandex.ru>
2023-04-24 18:12:52 +0300

    refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -4,93 +4,96 @@ const exit = document.getElementById('exit');
 const infoText = document.querySelectorAll('.user-info');
 
 checkJwt()
-    .then(user => authorizeUser(user))
-    .catch(user => noAuthorizeUser(user))
+	.then(user => authorizeUser(user))
+	.catch(() => noAuthorizeUser())
 
 exit.addEventListener('click', (e) => {
-    localStorage.removeItem('jwt')
-    window.location.href = '/sign_in';
+	localStorage.removeItem('jwt');
+	window.location.href = '/sign_in';
 })
+
 function getJwt(login, password) {
-    return new Promise((res, rej) => {
-        const userObj = {
-            Login: login,
-            Password: password
-        }
-        const dataUser = JSON.stringify(userObj);
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const JwtObj = JSON.parse(xhr.response);
-                if (JwtObj.Error !== null) {
-                    rej(JwtObj.Error)
-                    return;
-                }
-                res(JwtObj.Jwt)
-            }
-        }
-        xhr.open('POST', '/api/auth');
-        xhr.send(dataUser);
-    })
+	return new Promise((res, rej) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', '/api/auth');
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				const JwtObj = JSON.parse(xhr.response);
+				if (JwtObj.Error !== null) {
+					rej(JwtObj.Error);
+					return;
+				}
+				res(JwtObj.Jwt);
+			}
+		};
+		xhr.send(JSON.stringify({
+			Login: login,
+			Password: password,
+		}));
+	})
 }
+
 function checkJwt() {
-    return new Promise((res, rej) => {
-        const xhr = new XMLHttpRequest();
-        const dataJwt = JSON.stringify({
-            Jwt: localStorage.getItem('jwt')
-        });
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const userObj = JSON.parse(xhr.response);
-                if (userObj == null) {
-                    rej(userObj);
-                }
-                else {
-                    res(userObj);
-                }
-            }
-        }
-        xhr.open('POST', '/api/auth/user')
-        xhr.send(dataJwt);
-    })
+	return new Promise((res, rej) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open('POST', '/api/auth/user');
+		xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				const userObj = JSON.parse(xhr.response);
+				if (userObj == null) {
+					rej();
+				} else {
+					res(userObj);
+				}
+			}
+		};
+		xhr.send(JSON.stringify({
+			Jwt: localStorage.getItem('jwt')
+		}));
+	})
 
 }
+
 function authorizeUser(UserObj) {
-    infoText.forEach((elem) => elem.innerText = `${UserObj.LastName} ${UserObj.FirstName}`);
-    userElems.forEach((elem) => elem.classList.remove('hidden'));
-    guestElems.forEach((elem) => elem.classList.add('hidden'));
+	infoText.forEach((elem) => elem.innerText = `${UserObj.LastName} ${UserObj.FirstName}`);
+	userElems.forEach((elem) => elem.classList.remove('hidden'));
+	guestElems.forEach((elem) => elem.classList.add('hidden'));
 }
-function noAuthorizeUser(userObj) {
-    guestElems.forEach((elem) => elem.classList.remove('hidden'));
-    userElems.forEach((elem) => elem.classList.add('hidden'));
-    if (location.pathname !== '/sign_in' && location.pathname !== '/registration') {
-        window.location.href = '/sign_in';
-    }
-}
-function getUser(id) {
-    return new Promise((res, rej) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function () {
-            if (this.readyState == 4) {
-                if (this.status == 200) {
-                    let userObj = JSON.parse(xhr.response);
-                    res(userObj);
-                }
-                else {
-                    rej(id);
-                }
-            }
-        }
-        xhr.open('GET', `/api/user/one?id=${id}`);
-        xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('jwt')}`);
-        xhr.send();
 
-    }).then((user) => {
-        console.log(`Id пользователя: ${user.Id}`)
-        console.log(`Имя пользователя: ${user.FirstName}`)
-        console.log(`Фамилия пользователя: ${user.LastName}`)
-        console.log(`Логин пользователя: ${user.Login}`)
-    })
-        .catch((id) => console.error(id))
+function noAuthorizeUser() {
+	guestElems.forEach((elem) => elem.classList.remove('hidden'));
+	userElems.forEach((elem) => elem.classList.add('hidden'));
+	if (location.pathname !== '/sign_in' && location.pathname !== '/registration') {
+		window.location.href = '/sign_in';
+	}
+}
+
+function getUser(id) {
+
+	return new Promise((res, rej) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open('GET', `/api/user/one?id=${id}`);
+		xhr.setRequestHeader('Authorization', `Bearer ${localStorage.getItem('jwt')}`);
+		xhr.onreadystatechange = function () {
+			if (this.readyState === 4) {
+				let user = JSON.parse(xhr.response);
+				if (this.status === 200 && user != null) {
+					res(user);
+				} else {
+					rej(id);
+				}
+			}
+		};
+		xhr.send();
+	})
+		.then((user) => {
+			console.log(`Id пользователя: ${user.Id}`)
+			console.log(`Имя пользователя: ${user.FirstName}`)
+			console.log(`Фамилия пользователя: ${user.LastName}`)
+			console.log(`Логин пользователя: ${user.Login}`)
+		})
+		.catch((id) => {
+			console.log(`Пользователь с id:${id} - не найден`)
+		});
 }
 
